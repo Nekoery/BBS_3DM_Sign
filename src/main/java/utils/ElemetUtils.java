ec246c459476f9add288d8cd03615ebfa99a2a5b
liy <liy@landfone.com.cn>
2021-04-13 15:38:04 +0800

    [V3]修改为快速定位版面链接，可改为配置定位方式(待修改)

--- src/main/java/utils/ElemetUtils.java
+++ src/main/java/utils/ElemetUtils.java
@@ -1,5 +1,7 @@
 package utils;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,30 +9,58 @@ import org.openqa.selenium.WebElement;
 import java.util.List;
 
 public class ElemetUtils {
-    public static List<WebElement> getElementByTagName(WebDriver driver,String tagName){
+    final static Logger logger = LogManager.getLogger(ElemetUtils.class);
+    public static List<WebElement> getElementsByTagName(WebDriver driver,String tagName){
         try{
             return driver.findElements(By.tagName(tagName));
         }catch (Exception e){
-            e.printStackTrace();
+            logger.error("未找到相应元素");
             return null;
         }
     }
 
-    public static List<WebElement> getElementByTagName(WebElement element,String tagName){
+    public static List<WebElement> getElementsByTagName(WebElement element,String tagName){
         try{
             return element.findElements(By.tagName(tagName));
         }catch (Exception e){
-            e.printStackTrace();
+            logger.error("未找到相应元素");
             return null;
         }
     }
 
-//    public static List<WebElement> getElementByClass(WebElement element,String tagName){
-//        try{
-//            return element.findElements(By.ByClassName(tagName));
-//        }catch (Exception e){
-//            e.printStackTrace();
-//            return null;
-//        }
-//    }
+    public static WebElement getElementByTagName(WebElement element,String tagName){
+        try{
+            return element.findElement(By.tagName(tagName));
+        }catch (Exception e){
+            logger.error("未找到相应元素");
+            return null;
+        }
+    }
+
+    public static WebElement getElementById(WebDriver driver,String id){
+        try{
+            return driver.findElement(By.id(id));
+        }catch (Exception e){
+            logger.error("未找到相应元素");
+            return null;
+        }
+    }
+
+    public static WebElement getElementByClass(WebElement element,String className){
+        try{
+            return element.findElement(By.className(className));
+        }catch (Exception e){
+            logger.error("未找到相应元素");
+            return null;
+        }
+    }
+
+    public static WebElement getElementByPath(WebElement element,String path){
+        try{
+            return element.findElement(By.xpath(path));
+        }catch (Exception e){
+            logger.error("未找到相应元素");
+            return null;
+        }
+    }
 }
--- src/main/java/utils/ReplyUtils.java
+++ src/main/java/utils/ReplyUtils.java
@@ -6,6 +6,8 @@ import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -52,9 +54,9 @@ public class ReplyUtils {
         logger.info("---------------通过版面回复主题---------------");
         driver.get(pageUrl);
         List<String> urls = new ArrayList<String>();
-        List<WebElement> webRootElements =ElemetUtils.getElementByTagName(driver,"tbody");
+        List<WebElement> webRootElements =ElemetUtils.getElementsByTagName(driver,"tbody");
         for (WebElement w : webRootElements){
-            List<WebElement> webBodyElements = ElemetUtils.getElementByTagName(w,"a");
+            List<WebElement> webBodyElements = ElemetUtils.getElementsByTagName(w,"a");
             for(WebElement w1 : webBodyElements){
                 if(w1.getAttribute("title").equals("新窗口打开") ||
                         w1.getAttribute("title").equals("有新回复 - 新窗口打开")){
@@ -83,6 +85,7 @@ public class ReplyUtils {
             logger.info("版面为空");
             return false;
         }
+        long startTime = System.currentTimeMillis();
         Iterator<Map.Entry<String,String>> iterator = pageMap.entrySet().iterator();
         List<String> urls = new ArrayList<String>();
         while (iterator.hasNext()){
@@ -90,25 +93,35 @@ public class ReplyUtils {
             String url ="https://bbs.3dmgame.com/" + p.getKey();
             logger.info("版面链接" + url);
             driver.get(url);
-            List<WebElement> webRootElements =ElemetUtils.getElementByTagName(driver,"tbody");
+            //首先获取列表
+            WebElement listElement = ElemetUtils.getElementById(driver,"threadlisttableid");
+            //寻找列表中tbody
+            List<WebElement> webRootElements =ElemetUtils.getElementsByTagName(listElement,"tbody");
             for (WebElement w : webRootElements){
-                List<WebElement> webBodyElements = ElemetUtils.getElementByTagName(w,"a");
-                for(WebElement w1 : webBodyElements){
-                    if(w1.getAttribute("title").equals("新窗口打开") ||
-                            w1.getAttribute("title").equals("有新回复 - 新窗口打开")){
-                        urls.add(w1.getAttribute("href"));
-                        logger.info("版面主题URL"+w1.getAttribute("href"));
-                    }
+                WebElement icn = ElemetUtils.getElementByPath(w,".//tr//td[1]//a");
+                if(icn == null){
+                    continue;
                 }
+                logger.info(icn.getAttribute("title"));
+                logger.info("版面主题URL"+icn.getAttribute("href"));
+//                List<WebElement> webBodyElements = ElemetUtils.getElementsByTagName(icn,"a");
+//                for(WebElement w1 : webBodyElements){
+//                    if(w1.getAttribute("title").equals("新窗口打开") ||
+//                            w1.getAttribute("title").equals("有新回复 - 新窗口打开")){
+//                        urls.add(w1.getAttribute("href"));
+//                        logger.info("版面主题URL"+w1.getAttribute("href"));
+//                    }
+//                }
             }
         }
 //        int max = Integer.valueOf(p.getValue()) > urls.size() ? urls.size() : Integer.valueOf(p.getValue());
         logger.info("需要回复版面主题数量:" + urls.size());
-        for(int i = 0 ; i <urls.size() ; i++){
-            logger.info("");
-            logger.info("已回复主题数量: + "+ i + "....................................................");
-            replayByUrl(urls.get(i),driver,1);
-        }
+        logger.info("查找时间为:" + (System.currentTimeMillis() - startTime));
+//        for(int i = 0 ; i <urls.size() ; i++){
+//            logger.info("");
+//            logger.info("已回复主题数量: + "+ i + "....................................................");
+//            replayByUrl(urls.get(i),driver,1);
+//        }
         return true;
     }
 
